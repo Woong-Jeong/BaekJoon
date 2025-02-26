48eb27f7fe172125804d90dbc7e6d1304c45520e
Woong-Jeong <jjwisfool@gmail.com>
2025-02-26 20:15:29 +0900

    fin

---
+++ 11654.c
@@ -0,0 +1,14 @@
+// 11654¹ø
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+    unsigned char input;
+
+    scanf("%c", &input);
+    int output = input;
+    printf("%d", output);
+
+    return 0;
+}
